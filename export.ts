1e376a576960554b99e5b2649f7bc3bc5c4a849d
Jeromy Cannon <jeromylewiscannon@gmail.com>
2025-02-18 16:14:40 +0000

    added dotenv and basic run script
    Signed-off-by: Jeromy Cannon <jeromylewiscannon@gmail.com>

---
+++ export.ts
@@ -0,0 +1,36 @@
+import 'dotenv/config';
+import * as process from 'process';
+
+function main(argv: string[]){
+//////////////////////////////////////////////////////////////////////
+////   Global Variables                              /////////////////
+//////////////////////////////////////////////////////////////////////
+    var content;  // holds site-wide content (gear names and stats, quests, etc)
+    var tavern;   // holds tavern data
+    var party;    // holds party data
+    var user;     // holds user's data
+    var tasksFromDb;          // holds user's tasks except for ...
+    var completedTodosFromDb; // completed To Do's
+
+
+//////////////////////////////////////////////////////////////////////
+////   Global Connection Variables      //////////////////////////////
+//////////////////////////////////////////////////////////////////////
+    var serverName               = 'Habitica'; // used in "loading" message
+    var serverUrl                = 'https://habitica.com/api/v3';
+    var serverPathContent        = '/content?language=en';
+    var serverPathTavern         = '/groups/habitrpg';
+    var serverPathParty          = '/groups/party';
+    var serverPathGuildBase      = '/groups?type=guilds';
+    var serverPathUser           = '/user';
+    var serverPathTasks          = '/tasks/user';
+    var serverPathCompletedTodos = '/tasks/user?type=_allCompletedTodos';
+    var clientId                 = 'd904bd62-da08-416b-a816-ba797c9ee265-DataDisplayTool';
+    var userId                   = process.env.HABITICA_USER;
+    var apiToken                 = process.env.API_TOKEN;
+    var debug                    = false;
+
+    console.log(`user: ${userId}, apiToken:${apiToken}`)
+
+}
+main(process.argv);
